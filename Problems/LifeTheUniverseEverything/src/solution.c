f04dd8e80e7c93fea453a52675b88df1f5b7a0ad
Gokul Reddy <gokul.reddy28@gmail.com>
2017-11-04 23:07:20 +0530

    solution(CodeChef) Life, TheUniverse and Everything Problem

---
+++ Problems/LifeTheUniverseEverything/src/solution.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    i=1;
+    while(i)
+    {
+        scanf("%d\n",&n);
+        if(n==42)
+        {
+            i=0;
+        }
+        else{printf("%d\n",n);}
+ 
+    }
+    return 0;
+}
+ 
